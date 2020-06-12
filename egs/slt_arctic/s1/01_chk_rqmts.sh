e3baff9c008f5f71e5b6e4c522fef7198dc3b127
Mohammed Salah Al-Radhi <cemohammedsalah@gmail.com>
2020-06-12 11:02:55 +0200

    Update

--- egs/slt_arctic/s1/01_chk_rqmts.sh
+++ egs/slt_arctic/s1/01_chk_rqmts.sh
@@ -24,10 +24,10 @@ command -v octave >/dev/null 2>&1 || { echo -e >&2 "\nOctave is not found, but c
 # festival
 current_working_dir=$(pwd)
 cd /usr/share/doc/festival/examples/
-sudo gunzip -f dumpfeats.gz
-sudo gunzip -f dumpfeats.sh.gz
-sudo chmod a+rx /usr/share/doc/festival/examples/dumpfeats
-sudo chmod a+rx /usr/share/doc/festival/examples/dumpfeats.sh
+gunzip -f dumpfeats.gz
+gunzip -f dumpfeats.sh.gz
+chmod a+rx /usr/share/doc/festival/examples/dumpfeats
+chmod a+rx /usr/share/doc/festival/examples/dumpfeats.sh
 cd $current_working_dir
 
 
